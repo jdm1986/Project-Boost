4002f070934028983a121ba724f7a25785f78b87
Josh Mix <joshuadmix@gmail.com>
2018-04-13 14:56:14 -0400

    Added force multipliers to thrust vectors. Changed rocket to dog.

--- RocketDude/Assets/rocket.cs
+++ RocketDude/Assets/rocket.cs
@@ -4,27 +4,53 @@ using UnityEngine;
 
 public class rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     private Rigidbody rigidBody;
     private AudioSource audioSource;
 
 	// Use this for initialization
-	void Start ()
+	private void Start ()
 	{
 	    rigidBody = GetComponent<Rigidbody>();
 	    audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	private void Update ()
 	{
 	    ProcessInput();
 	}
 
     private void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.back);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
+    }
+
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -33,15 +59,6 @@ public class rocket : MonoBehaviour
         else
         {
             audioSource.Stop();
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.back);
         }
     }
 }
